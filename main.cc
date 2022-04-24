95ce36c7afe639f71dae9a52489e9abe9397ff9a
lvboqi <925220146@qq.com>
2022-04-24 18:29:54 +0200

    fix conflict

--- main.cc
+++ main.cc
@@ -1,12 +1,18 @@
 /* Add your code here */ 
 
+
+// void test_set_led(){
+
+// 	kc.set_led(4, 1);
+
+// }
 int main()
 {
 /* Add your code here */ 
- 
+
 /* Add your code here */ 
- 
+
 /* Add your code here */ 
- 
+
 	return 0;
 }
