b71d7e4cf776439aff10db466abc1e498a3e9e5f
lvboqizw <925220146@qq.com>
2022-06-22 20:32:44 +0200

    add some annotation on the methods of the previous tasks

--- thread/dispatch.h
+++ thread/dispatch.h
@@ -25,10 +25,13 @@ private:
 	Coroutine *ptr;
 
 public:
-
+	// The constructor initializes the life pointer with null to indicate that no coroutine is known yet.
 	Dispatcher();
+	// With this method the coroutine first is put in the life pointer and started.
 	void go(Coroutine &first);
+	// This method sets the life pointer to next and performs a coroutine switch from the old to the new life pointer.
 	void dispatch(Coroutine &next);
+	// This can be used to determine which coroutine is currently in control of the processor.
 	Coroutine *active();
 };
 
--- thread/scheduler.h
+++ thread/scheduler.h
@@ -28,10 +28,18 @@ private:
 public:
 	Scheduler(){};
 
+	// This method registers the process that with the scheduler. It is appended to the end of the ready list.
 	void ready(Entrant &that);
+	// This method starts up scheduling by removing the first process from the ready list and activating it.
 	void schedule();
+	// With this method a process can terminate itself.
 	void exit();
+	// With this method a process can terminate another one (that).
 	void kill(Entrant &that);
+	 /** This method allows to trigger a context switch without the calling
+	  *  Entrant having to know which other Entrant objects exist in the system, 
+	  * and which of these should be activated. 
+	  * */
 	void resume();
  
 };
